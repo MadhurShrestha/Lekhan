36bedc4e408e04d8ac1b659717b6b4962170374a
Aayush <Aayushstha25@gmail.com>
2022-02-11 22:58:50 +0545

    Upgraded some gems and added destroy relationships

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,6 +1,6 @@
 class Checklist < ApplicationRecord
   belongs_to :page
-  has_many :checklist_items
+  has_many :checklist_items, dependent: :destroy
 
   def new_item
     checklist_items.build
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,8 @@
 class Page < ApplicationRecord
   belongs_to :notebook
-  has_many :paragraphs
-  has_many :image_elements
-  has_many :checklists
+  has_many :paragraphs, dependent: :destroy
+  has_many :image_elements, dependent: :destroy
+  has_many :checklists, dependent: :destroy
 
   def elements
     paragraphs + image_elements + checklists
