16ea55091ee0d407769177720351d0779257cf3a
Aayush <Aayushstha25@gmail.com>
2022-04-19 01:55:30 +0545

    to run pagy gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_breadcrumbs
-
+  include Pagy::Backend
 
   def configure_permitted_parameters
     added_attrs = [:username, :email, :avatar, :password_confimration, :remember_me]
